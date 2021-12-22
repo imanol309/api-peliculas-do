0223e41e7653ae6ab54e176890da7771d2ecc5c1
imanol309 <imanolguzman309@gmail.com>
2021-12-22 17:02:53 -0400

    cambiando la estrctura de la base de datos el modelo

--- models/EstructuraDeBD.js
+++ models/EstructuraDeBD.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const mascotaSchema = new Schema({
-  nombre: String,
-  edad: String,
-  color: String,
+  titulo: String,
+  genero: String,
+  Director: String,
+  Reparto: String,
 });
 
 // crear el modelo
 
-const Mascota = mongoose.model("Mascota", mascotaSchema);
+const Mascota = mongoose.model("dominicanas", mascotaSchema);
 
 module.exports = {
   Mascota,
