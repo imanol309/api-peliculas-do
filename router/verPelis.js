1bd5c9b6b343219b26d94e2f89a8505682336720
imanol309 <imanolguzman309@gmail.com>
2022-01-27 12:14:45 -0400

    Documentado las rutas de las peliculas de GET

--- router/verPelis.js
+++ router/verPelis.js
@@ -9,6 +9,7 @@ routerVer.get(`/`, (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+// RUTA PARA BUSCAR LA PELICULA POR EL ID
 routerVer.get("/:id", async (req, res) => {
   const id = req.params.id;
   await Mascota.findOne({ _id: id })
@@ -16,6 +17,7 @@ routerVer.get("/:id", async (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+// RUTA PARA BUSCAR LA PELICULA POR EL NOMBRE DE LA PELI
 routerVer.get("/titulo/:nombre", async (req, res) => {
   const id = req.params.nombre;
   const titulos = id.split("-").join(" ");
@@ -24,6 +26,7 @@ routerVer.get("/titulo/:nombre", async (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+// RUTA PARA BUSCAR LA PELICULA POR EL YEARS, PERO TIENE UN PEQUEÑO BUG(FALTA SOLUCIONAR)
 routerVer.get("/fecha/:years", async (req, res) => {
   const fecha = req.params.years;
   await Mascota.find({ año: fecha })
@@ -31,13 +34,16 @@ routerVer.get("/fecha/:years", async (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+// RUTA PARA BUSCAR LAS PELICULAS POR EL GENERO DE LA PELICULA
 routerVer.get("/genero/:nombre", async (req, res) => {
   const generos = req.params.nombre;
+  const nombreSG = id.split("-").join(" ");
   await Mascota.find({ genero: generos })
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
 
+// DONDE EXPORTO LAS PELICULAS
 module.exports = {
   routerVer,
 };
