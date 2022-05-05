246d318220f4ae02abc0a6ad5746935cd892fcbf
imanol309 <imanolguzman309@gmail.com>
2022-05-05 12:12:44 -0400

    Agregando de donde puedo hacer peticiones de cuales host

--- servidor.js
+++ servidor.js
@@ -21,7 +21,7 @@ const { isAuth, isAuthSecret } = require("./middlewares/auth");
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888' || 'https://films-do-ng.vercel.app');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
